9d14e8c5f6e138433c27193c6b08a9d5e124c0d5
Camilo Cunha de Azevedo <30880723+Camilotk@users.noreply.github.com>
2017-11-10 02:25:43 -0200

    Create blondel.hs

---
+++ arquitetura/blondel.hs
@@ -0,0 +1,6 @@
+blondel x y = if (1*x + y == 63) || (2*x + y ==64) 
+                  then do putStrLn "O valor está correto!"
+                  else do putStrLn "O valor não fecha :'("
+
+blondelEspelhos p = (63 - p)/2
+numeroEspelhos h e = h/e 
